08a4a8c4cac5e501aa6ade89f415f67838ec35cb
Rabiou <mroumanesouley@gmail.com>
2024-01-16 16:22:32 +0100

    Initial Commit

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface BookRepository {
+    Iterable<Book> findAll();
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+    Book save(Book book);
+    void deleteByIsbn(String isbn);
+}
